48ed2c5bdf7a94ab2e4f65a25ac888f53006c486
aking <aking@546ca710-db30-0410-8887-d9dd8e66aff0>
2009-02-01 06:21:54 +0000

    adding site redirect plugin
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-plugins/cf-site-redirect/trunk@7431 546ca710-db30-0410-8887-d9dd8e66aff0

---
+++ cf-site-redirect.php
@@ -0,0 +1,31 @@
+<?php
+/* 
+Plugin Name: CF Site Redirect
+Description: Redirect old url requests to the WordPress equivalent
+Version: 1.0
+Author: Crowd Favorite
+Author URI: http://crowdfavorite.com/
+*/
+
+/**
+ * look for the url as a post meta field and redirect to that post if found
+ */
+define('LEGACY_URL_FIELD_NAME','_legacy_url');
+
+function cf_site_redirect() {
+	if ( is_404() ) {
+		global $wpdb;
+		$req = $_SERVER['REQUEST_URI'];
+		
+		$query = "SELECT * FROM $wpdb->postmeta WHERE meta_key = '".LEGACY_URL_FIELD_NAME."' AND meta_value LIKE '%".$wpdb->escape($req)."%'";
+		$result = $wpdb->get_row($query);
+
+		if (!empty($result)) {
+			wp_redirect(get_permalink($result->post_id), 301);
+			exit();
+		}
+	}
+}
+add_action('template_redirect','cf_site_redirect');
+
+?>
