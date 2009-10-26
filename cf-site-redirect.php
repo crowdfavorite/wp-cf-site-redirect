19d7a290da000fc6fa1b50d569dc84d4fab62bb6
sparker <sparker@546ca710-db30-0410-8887-d9dd8e66aff0>
2009-10-26 22:59:42 +0000

    fixing issue with partial matching. Now restricts match to match $_SERVER['REQUEST_URI'] to the end of the original_url field
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-plugins/cf-site-redirect/tags/1.0@17589 546ca710-db30-0410-8887-d9dd8e66aff0

--- cf-site-redirect.php
+++ cf-site-redirect.php
@@ -17,7 +17,7 @@ function cf_site_redirect() {
 		global $wpdb;
 		$req = $_SERVER['REQUEST_URI'];
 		
-		$query = "SELECT * FROM $wpdb->postmeta WHERE meta_key = '".LEGACY_URL_FIELD_NAME."' AND meta_value LIKE '%".$wpdb->escape($req)."%'";
+		$query = "SELECT * FROM $wpdb->postmeta WHERE meta_key = '".LEGACY_URL_FIELD_NAME."' AND meta_value LIKE '%".$wpdb->escape($req)."'";
 		$result = $wpdb->get_row($query);
 
 		if (!empty($result)) {
